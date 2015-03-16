43aa2769d7d25abe301856d7f479d17f36bcbfe7
Reed S <rshin808@yahoo.com>
2015-03-16 11:58:29 -1000

    test

--- TestSite/CSBCD/temperatures/views.py
+++ TestSite/CSBCD/temperatures/views.py
@@ -4,9 +4,16 @@ from .models import Temperature
 import simplejson
 
 def chart_data_json(request):
-    data = {}
+    temperatures = Temperature.objects.all()
+    to_json = []
+
+    count = 0
+    for temp in temperatures:
+        temp_dict['dates'] = count
+        temp_dict['values'] = temp.tempF
+        to_json.append(temp_dict)
     
-    data['chart_data'] = ChartData.get_tempF()
+    response_data = simplejson.dumps(to_json)
   
-    return HttpResponse(json.dumps(data), content_type = 'application/json') 
+    return HttpResponse(response_data, content_type = 'application/json') 
 
