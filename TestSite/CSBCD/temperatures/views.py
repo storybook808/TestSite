05f26616ad55401e256e1fe574a48c2a1dc10766
rshin808 <rshin808@gmail.com>
2015-04-08 13:41:35 -1000

    adding

--- TestSite/CSBCD/temperatures/views.py
+++ TestSite/CSBCD/temperatures/views.py
@@ -4,9 +4,11 @@ from .models import Temperature
 import json
 
 def chart_data_json(request):
-    to_json = {}
-    to_json['dates'] = 0;
-    to_json['values'] = 1;
-
-    return HttpResponse(json.dumps(to_json), mimetype = 'application/json') 
-
+    to_json = []
+    for i in range(10):
+        data_dict = {}
+        data_dict['timestamp'] = i
+        data_dict['values'] = i
+        to_json.append(data_dict)        
+    response_data = json.dumps(to_json)
+    return HttpResponse(response_data, content_type="application/json")
